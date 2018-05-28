eb45fb3ec1d2729c91932ecf01211759e034e86d
derrick b <dbozich1@gmail.com>
2018-05-28 03:25:53 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,17 +5,14 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
   array = text.scan(/\bun\w+ing\b/)
   array
-
-  
-
-
 end
 
 def words_five_letters_long(text)
-  /\b\w{5}\b/
+  array = text.scan(/\b\w{5}\b/)
+  array
+  
 
 end
 
