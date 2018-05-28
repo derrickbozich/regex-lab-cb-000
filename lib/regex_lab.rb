791469834fc4adfd85dd22451b6fc578bfc4a4b2
derrick b <dbozich1@gmail.com>
2018-05-28 03:14:02 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,25 @@
 def starts_with_a_vowel?(word)
+  /\b[aeiou]/
 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  /\bun\w+ing\b/
+  
 
 end
 
 def words_five_letters_long(text)
+  /\b\w{5}\b/
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  if /\b[A-Z][.!?]\b/
 
 end
 
 def valid_phone_number?(phone)
+  /\b\d{3}-\d{3}-\d{4}/
 
 end
