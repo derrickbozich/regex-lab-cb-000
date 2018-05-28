e0b66b9c68dd199c48fbf3b6ca2486a857c8cd3e
derrick b <dbozich1@gmail.com>
2018-05-28 03:35:54 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -22,7 +22,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/\b\d{3}-\d{3}-\d{4}/)
+  if phone.match(/\(?\d{3}\)?[-.\s]\d{3}[-.\s]\d{4}/)
     true
   else
     false
